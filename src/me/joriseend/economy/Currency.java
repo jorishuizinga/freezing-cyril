a769f0978ff5f0baf41a6208b0b1615c4a185979
Joris <jorishuizinga@me.com>
2015-07-09 15:06:28 +0200

    Numerous edits

--- src/me/joriseend/economy/Currency.java
+++ src/me/joriseend/economy/Currency.java
@@ -16,9 +16,10 @@ public class Currency extends JavaPlugin {
 	String FewArgsPlayer = "Please specify a player!";
 	String FewArgsAmount = "Please specify how much";
 	String IncorrectBalance = EconomyPrefix + ChatColor.RED + "Your account has insufficient funds.";
+	String ActualMoney = EconomyPrefix + ChatColor.RED + "You cannot send negative money";
 	String YouGotMoney = EconomyPrefix + ChatColor.GREEN + "You recieved" + " ";
 	String YouLostMoney = ChatColor.GRAY + " " + "has been deducted from your account";
-	String MoneyAddSuccess = EconomyPrefix + "Successfully added" + " ";
+	String YouNowHaveDifferentMoney = EconomyPrefix + ChatColor.GRAY + "Your balance is now" + " " + ChatColor.RESET;
 	String OnlinePlayerFindPrefix = ChatColor.DARK_GRAY + "Online Player Search>" + ChatColor.RESET + " ";
 	String OnlinePlayerFindConsolePrefix = "[Online Player Search]" + " ";
 	String CannotFindPlayer = ChatColor.RED + "Cannot find player" + ChatColor.RESET;
@@ -49,17 +50,28 @@ public class Currency extends JavaPlugin {
 					player.sendMessage(EconomyPrefix + FewArgsAmount);
 					return true;
 				}else if(args.length == 2){
-					int amount = Integer.parseInt(args[1]);
-					Player target = Bukkit.getPlayer(args[0]);
-					if(getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) >= amount){
-						getConfig().set(player.getUniqueId().toString() + ConfigBalance, getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) - amount);
-						getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) + amount);
-						target.sendMessage(YouGotMoney + ChatColor.GOLD + amount);
-						player.sendMessage(EconomyPrefix + ChatColor.GOLD + amount + YouLostMoney);
+					if(Bukkit.getPlayer(args[0]) == null){
+						player.sendMessage(OnlinePlayerFindPrefix + CannotFindPlayer);
 						return true;
-					}else if(getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) < amount){
-						player.sendMessage(IncorrectBalance);
-						return true;
+					}else{
+						int amount = Integer.parseInt(args[1]);
+						Player target = Bukkit.getPlayer(args[0]);
+						if(amount <= 0){
+							player.sendMessage(ActualMoney);
+							return true;
+						}else{
+							if(getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) >= amount){
+								getConfig().set(player.getUniqueId().toString() + ConfigBalance, getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) - amount);
+								getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) + amount);
+								saveConfig();
+								target.sendMessage(YouGotMoney + ChatColor.GOLD + amount);
+								player.sendMessage(EconomyPrefix + ChatColor.GOLD + amount + YouLostMoney);
+								return true;
+							}else if(getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) < amount){
+								player.sendMessage(IncorrectBalance);
+								return true;
+							}
+						}
 					}
 				}else if(args.length > 2){
 					player.sendMessage(TooManyArgs);
@@ -70,6 +82,7 @@ public class Currency extends JavaPlugin {
 				}
 			}else{
 				//TODO Execute when sender is console
+				return true;
 			}
 		}else if(command.getName().equalsIgnoreCase("balance")){
 			if(sender instanceof Player){
@@ -79,6 +92,7 @@ public class Currency extends JavaPlugin {
 				return true;
 			}else{
 				//TODO Execute when sender is console
+				return true;
 			}
 		}else if(command.getName().equalsIgnoreCase("addbalance")){
 			if(sender instanceof Player){
@@ -98,8 +112,9 @@ public class Currency extends JavaPlugin {
 							Player target = Bukkit.getPlayer(args[0]);
 							int amount = Integer.parseInt(args[1]);
 							getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) + amount);
+							saveConfig();
 							target.sendMessage(YouGotMoney + ChatColor.GOLD + amount);
-							player.sendMessage(MoneyAddSuccess + ChatColor.GOLD + amount + ChatColor.RESET + " " + "to" + " " + ChatColor.GOLD + target.getDisplayName() + ChatColor.RESET + "'s" + " " + "balance");
+							player.sendMessage(EconomyPrefix + "Successfully added" + " " + ChatColor.GOLD + amount + ChatColor.RESET + " " + "to" + " " + ChatColor.GOLD + target.getDisplayName() + ChatColor.RESET + "'s" + " " + "balance");
 							return true;
 						}
 					}else if(args.length > 2){
@@ -109,7 +124,13 @@ public class Currency extends JavaPlugin {
 						player.sendMessage(EconomyPrefix + error);
 						return true;
 					}
+				}else{
+					player.sendMessage(NoPermission);
+					return true;
 				}
+			}else{
+				//TODO Execute when sender is console
+				return true;
 			}
 		}else if(command.getName().equalsIgnoreCase("rembalance")){
 			if(sender instanceof Player){
@@ -129,7 +150,10 @@ public class Currency extends JavaPlugin {
 							Player target = Bukkit.getPlayer(args[0]);
 							int amount = Integer.parseInt(args[1]);
 							getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) - amount);
+							saveConfig();
 							target.sendMessage(EconomyPrefix + ChatColor.GOLD + amount + YouLostMoney);
+							player.sendMessage(EconomyPrefix + "Successfull deducted" + " " + ChatColor.GOLD + amount + ChatColor.RESET + " " + "from" + " " + ChatColor.GOLD + target.getDisplayName() + ChatColor.RESET + "'s" + " " + "balance");
+							return true;
 						}
 					}else if(args.length > 2){
 						player.sendMessage(TooManyArgs);
@@ -138,7 +162,50 @@ public class Currency extends JavaPlugin {
 						player.sendMessage(EconomyPrefix + error);
 						return true;
 					}
+				}else{
+					player.sendMessage(NoPermission);
+					return true;
 				}
+			}else{
+				//TODO Execute when sender is console
+				return true;
+			}
+		}else if(command.getName().equalsIgnoreCase("setbalance")){
+			if(sender instanceof Player){
+				Player player = (Player) sender;
+				if(player.hasPermission("economy.manage.setbalance")){
+					if(args.length == 0){
+						player.sendMessage(EconomyPrefix + ChatColor.RED + FewArgsPlayer);
+						return true;
+					}else if(args.length == 1){
+						player.sendMessage(EconomyPrefix + ChatColor.RED + FewArgsAmount);
+						return true;
+					}else if(args.length == 2){
+						if(Bukkit.getPlayer(args[0]) == null){
+							player.sendMessage(OnlinePlayerFindPrefix + CannotFindPlayer);
+							return true;
+						}else{
+							Player target = Bukkit.getPlayer(args[0]);
+							int amount = Integer.parseInt(args[1]);
+							getConfig().set(target.getUniqueId().toString() + ConfigBalance, amount);
+							saveConfig();
+							target.sendMessage(YouNowHaveDifferentMoney + ChatColor.GOLD + amount);
+							player.sendMessage(EconomyPrefix + "Successfully set the balance of" + " " + ChatColor.GOLD + target.getDisplayName() + ChatColor.RESET + " " + "to" + " " + ChatColor.GOLD + amount);
+							return true;
+						}
+					}else if(args.length > 2){
+						player.sendMessage(TooManyArgs);
+						return true;
+					}else{
+						player.sendMessage(EconomyPrefix + error);
+					}
+				}else{
+					player.sendMessage(NoPermission);
+					return true;
+				}
+			}else{
+				//TODO Execute when sender is console
+				return true;
 			}
 		}
 		return false;
