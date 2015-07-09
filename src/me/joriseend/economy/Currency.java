28190d3c4532dc3d0d556eb2ca40b9e76fc428fb
Joris <jorishuizinga@me.com>
2015-07-09 13:51:33 +0200

    Fixed things
    Changed "YouLostMessage" formatting
    Added argument and Player checking to the addbalance command
    Added rembalance command for removing balance from someone's account

--- src/me/joriseend/economy/Currency.java
+++ src/me/joriseend/economy/Currency.java
@@ -17,12 +17,12 @@ public class Currency extends JavaPlugin {
 	String FewArgsAmount = "Please specify how much";
 	String IncorrectBalance = EconomyPrefix + ChatColor.RED + "Your account has insufficient funds.";
 	String YouGotMoney = EconomyPrefix + ChatColor.GREEN + "You recieved" + " ";
-	String YouLostMoney = EconomyPrefix + ChatColor.GRAY + "You sent" + " ";
+	String YouLostMoney = ChatColor.GRAY + " " + "has been deducted from your account";
 	String MoneyAddSuccess = EconomyPrefix + "Successfully added" + " ";
 	String OnlinePlayerFindPrefix = ChatColor.DARK_GRAY + "Online Player Search>" + ChatColor.RESET + " ";
 	String OnlinePlayerFindConsolePrefix = "[Online Player Search]" + " ";
 	String CannotFindPlayer = ChatColor.RED + "Cannot find player" + ChatColor.RESET;
-	String TooManyArgs = ChatColor.RED + "Too many arguments!" + " " + ChatColor.DARK_RED + "DENIED!";
+	String TooManyArgs = EconomyPrefix + ChatColor.RED + "Too many arguments!" + " " + ChatColor.DARK_RED + "DENIED!";
 	String NoPermission = EconomyPrefix + ChatColor.RED + "Many permissions, such denied, much no. Wow.";
 	String error = ChatColor.DARK_RED + "euhm... something went wrong";
 	String ConfigBalance = ".Balance";
@@ -55,7 +55,7 @@ public class Currency extends JavaPlugin {
 						getConfig().set(player.getUniqueId().toString() + ConfigBalance, getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) - amount);
 						getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) + amount);
 						target.sendMessage(YouGotMoney + ChatColor.GOLD + amount);
-						player.sendMessage(YouLostMoney + ChatColor.GOLD + " " + "to" + " " + target.getDisplayName());
+						player.sendMessage(EconomyPrefix + ChatColor.GOLD + amount + YouLostMoney);
 						return true;
 					}else if(getConfig().getInt(player.getUniqueId().toString() + ConfigBalance) < amount){
 						player.sendMessage(IncorrectBalance);
@@ -84,12 +84,54 @@ public class Currency extends JavaPlugin {
 			if(sender instanceof Player){
 				Player player = (Player) sender;
 				if(player.hasPermission("economy.manage.addbalance")){
-					Player target = Bukkit.getPlayer(args[0]);
-					int amount = Integer.parseInt(args[1]);
-					getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) + amount);
-					target.sendMessage(YouGotMoney + ChatColor.GOLD + amount);
-					player.sendMessage(MoneyAddSuccess + ChatColor.GOLD + amount + ChatColor.RESET + " " + "to" + " " + ChatColor.GOLD + target.getDisplayName() + ChatColor.RESET + "'s" + " " + "balance");
-					return true;
+					if(args.length == 0){
+						player.sendMessage(EconomyPrefix + ChatColor.RED + FewArgsPlayer);
+						return true;
+					}else if(args.length == 1){
+						player.sendMessage(EconomyPrefix + ChatColor.RED + FewArgsAmount);
+						return true;
+					}else if(args.length == 2){
+						if(Bukkit.getPlayer(args[0]) == null){
+							player.sendMessage(OnlinePlayerFindPrefix + CannotFindPlayer);
+							return true;
+						}else{
+							Player target = Bukkit.getPlayer(args[0]);
+							int amount = Integer.parseInt(args[1]);
+							getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) + amount);
+							target.sendMessage(YouGotMoney + ChatColor.GOLD + amount);
+							player.sendMessage(MoneyAddSuccess + ChatColor.GOLD + amount + ChatColor.RESET + " " + "to" + " " + ChatColor.GOLD + target.getDisplayName() + ChatColor.RESET + "'s" + " " + "balance");
+							return true;
+						}
+					}else if(args.length > 2){
+						player.sendMessage(TooManyArgs);
+						return true;
+					}else{
+						player.sendMessage(EconomyPrefix + error);
+						return true;
+					}
+				}
+			}
+		}else if(command.getName().equalsIgnoreCase("rembalance")){
+			if(sender instanceof Player){
+				Player player = (Player) sender;
+				if(player.hasPermission("economy.manage.rembalance")){
+					if(args.length == 0){
+						player.sendMessage(EconomyPrefix + ChatColor.RED + FewArgsPlayer);
+						return true;
+					}else if(args.length == 1){
+						player.sendMessage(EconomyPrefix + ChatColor.RED + FewArgsAmount);
+						return true;
+					}else if(args.length == 2){
+						if(Bukkit.getPlayer(args[0]) == null){
+							player.sendMessage(OnlinePlayerFindPrefix + CannotFindPlayer);
+							return true;
+						}else{
+							Player target = Bukkit.getPlayer(args[0]);
+							int amount = Integer.parseInt(args[1]);
+							getConfig().set(target.getUniqueId().toString() + ConfigBalance, getConfig().getInt(target.getUniqueId().toString() + ConfigBalance) - amount);
+							target.sendMessage(EconomyPrefix + ChatColor.GOLD + amount + YouLostMoney);
+						}
+					}
 				}
 			}
 		}
